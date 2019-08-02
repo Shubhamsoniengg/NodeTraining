c164dc0eee19819d16aa2a0cc5a2b744ee1883b0
Neha Soni <nehas@successive.com>
2019-08-02 19:34:42 +0530

    Install Nodemone

---
+++ extra/extraTs/interfaces.ts
@@ -0,0 +1,13 @@
+export interface IPermissions {
+    [GetUsers: string]: {
+      all: string[];
+      read: string[];
+      write: string[];
+      delete: string[];
+    };
+  }
+  export interface IUsers {
+    traineeEmail: string;
+    reviewerEmail: string;
+  }
+  
